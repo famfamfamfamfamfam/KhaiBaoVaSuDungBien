fdd45c40179182399a34577940193d44a1501fb7
BinhTran <fam.out16@gmail.com>
2024-08-06 05:04:22 +0700

    Add cac files cua bai thuc hanh khai bao va su dung bien

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+namespace KHAIBAOVASUDUNGBIEN
+{
+    class KhaiBaoBienVaInRaManHinh
+    {
+        static void Main(string[] args)
+        {
+            int i = 1;
+            float f = 20.5f;
+            double d = 20.5;
+            Boolean b = true;
+            char c = 'a';
+            string s = "CodeGym";
+            Console.WriteLine("Gia tri cua bien i la: " + i);
+            Console.WriteLine("Gia tri cua bien f la: " + f);
+            Console.WriteLine("Gia tri cua bien d la: " + d);
+            Console.WriteLine("Gia tri cua bien b la: " + b);
+            Console.WriteLine("Gia tri cua bien c la: " + c);
+            Console.WriteLine("Gia tri cua bien s la: " + s);
+        }
+    }
+}
